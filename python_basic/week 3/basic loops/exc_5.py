cdef112ca22a6410dac99fd067427dc78437172a
wojtek239 <wojtekopelt1@wp.pl>
2023-05-17 20:14:16 +0200

    corrections in exc5 bool

--- python_basic/week 3/basic loops/exc_5.py
+++ python_basic/week 3/basic loops/exc_5.py
@@ -2,3 +2,9 @@ k = int(input("please enter k: "))
 for i in range (50, 101):
     if i % k == 0:
         print(i)
+
+u = int(input("please enter opening interval: "))
+u2 = int(input("please enter closing interval: "))
+for i in range (u, u2):
+    if i % (k) == 0:
+        print(i)
