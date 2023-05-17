1b3f4714664f42ab8ec49b57ad64a8e812a6465f
wojtek239 <wojtekopelt1@wp.pl>
2023-05-17 22:05:16 +0200

    exc6 condit

--- python_basic/week 3/conditional statements/exc_6.py
+++ python_basic/week 3/conditional statements/exc_6.py
@@ -1,9 +1,13 @@
 import random
-players_choice = input("please enter your guess: heads'1' or tails'0': ")
+players_choice = int(input("please enter your guess: heads'1' or tails'0': "))
 if players_choice != 0 and players_choice != 1:
     print("only chose 0 or 1")
 1 == "heads"
 0 == "tails"
 throw = random.randint(0, 1)
 print(f" 3...2...1...\n {throw}")
+if 0:
+    print("tails")
+elif 1:
+    print("heads")
 
