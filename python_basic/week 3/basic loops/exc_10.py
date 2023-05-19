41053b317c9901935221574cb337f70b7fe26d34
wojtek239 <wojtekopelt1@wp.pl>
2023-05-19 15:26:36 +0200

    .

--- python_basic/week 3/basic loops/exc_10.py
+++
@@ -1,7 +0,0 @@
-user_number = int(input("please enter number: "))
-dividors_sum = 0
-def perfect_number(num):
-    for i in range(1, num):
-        if num % 1 == 0:
-            dividors_sum += i
-# everytime there is no change while dividing, dividors_sum grows by 1
