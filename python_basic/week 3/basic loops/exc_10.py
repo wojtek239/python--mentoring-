69a4e7ab3fd69b154056fc938daf490784260145
wojtek239 <wojtekopelt1@wp.pl>
2023-05-19 15:15:14 +0200

    mayby im lost

--- python_basic/week 3/basic loops/exc_10.py
+++ python_basic/week 3/basic loops/exc_10.py
@@ -4,4 +4,4 @@ def perfect_number(num):
     for i in range(1, num):
         if num % 1 == 0:
             dividors_sum += i
-# everytime there is no change while dividing, dividors_sum grows
+# everytime there is no change while dividing, dividors_sum grows by 1
