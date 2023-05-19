6677387fed493e8be015109a041bdfa6c775e7cc
wojtek239 <wojtekopelt1@wp.pl>
2023-05-19 14:16:52 +0200

    more exc

--- python_basic/week 3/conditional statements/exc_6.py
+++ python_basic/week 3/conditional statements/exc_6.py
@@ -14,7 +14,7 @@ while players_choice == "1":
 #    print("only choose 0 or 1.")
 #    break
 computer_choice = random.randint(0, 1)
-print("3...2...1... throw ! ")
+
 while computer_choice == 0:
     print("computer choose eagle.")
     break
@@ -24,5 +24,16 @@ while computer_choice == 1:
 while players_choice == "0" and computer_choice == "0":
         print("draw")
         break
-
-
+print("3...2...1... throw ! ")
+throw = random.randint(0, 1)
+print(f'{throw} was thrown, which means: ')
+if throw == 0:
+    print("eagle")
+if throw == 1:
+    print("tails")
+while players_choice == 0 and throw == 0:
+    print("player scores a point !")
+    break
+while computer_choice == 1 and throw == 1:
+    print("computer scores a point !")
+    break
