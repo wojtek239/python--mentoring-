d65e757154b8fe4cae7549cd8d9c12957f8a1240
wojtek239 <wojtekopelt1@wp.pl>
2023-05-22 20:02:07 +0200

    ex 3

--- python_basic/week 3/conditional statements/exc_4.py
+++ python_basic/week 3/conditional statements/exc_4.py
@@ -1,21 +1,21 @@
 input1 = input("player1 enter rock/paper/scissors: ")
 input2 = input("player2 enter rock/paper/scissors: ")
-if input1 == "rock" and input2 == "paper":
-    print("player2 wins")
-elif input1 == "rock" and input2 == "scissors":
-    print("player1 wins")
-elif input1 == input2:
-    print("draw")
-elif input1 == "paper" and input2 == "scissors":
-    print("player2 wins")
-elif input1 == "paper" and input2 == "rock":
-    print("player1 wins")
-elif input1 == "scissors" and input2 == "rock":
-    print("player2 wins")
-elif input1 == "scissors" and input2 == "paper":
-    print("player1 wins")
-else:
-    print("wrong data")
+#if input1 == "rock" and input2 == "paper":
+#    print("player2 wins")
+#elif input1 == "rock" and input2 == "scissors":
+#    print("player1 wins")
+#elif input1 == input2:
+#    print("draw")
+#elif input1 == "paper" and input2 == "scissors":
+#    print("player2 wins")
+#elif input1 == "paper" and input2 == "rock":
+#    print("player1 wins")
+#elif input1 == "scissors" and input2 == "rock":
+#    print("player2 wins")
+#elif input1 == "scissors" and input2 == "paper":
+#    print("player1 wins")
+#else:
+#    print("wrong data")
 
 choices = ['rock', 'paper', 'scissors']
 
