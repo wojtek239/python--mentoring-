d16348e826fac1b8d9c0379b1e7fda472d1ffeb0
wojtek239 <wojtekopelt1@wp.pl>
2023-05-23 11:19:28 +0200

    finished with exc7

--- python_basic/week_4/training4-6/exc_7.py
+++ python_basic/week_4/training4-6/exc_7.py
@@ -1,1 +1,13 @@
-git
+user_number = int(input("please enter ur number: "))
+if user_number >= 0:
+    print(user_number)
+
+elif user_number < 0:
+    print(-user_number)
+
+else:
+    print("only type number !")
+
+
+
+
