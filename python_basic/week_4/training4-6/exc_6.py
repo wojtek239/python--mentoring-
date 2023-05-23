3622e2198eb2be88d42e124391001da6d88758c9
wojtek239 <wojtekopelt1@wp.pl>
2023-05-23 11:03:22 +0200

    finished with exc6

---
+++ python_basic/week_4/training4-6/exc_6.py
@@ -0,0 +1,23 @@
+a = [3, 1, 5, 7, 9, 2, 6]
+print(a[3])
+# element with third index
+print(a[1:4])
+# elements with index from 1 to 4
+print(a[3:])
+# every element after index 3 ounting in
+print(a[-3:])
+#every element from third index backwards
+print(a[:3])
+#
+print(a[3:-1])
+#
+print(a[::2])
+# every second element
+print(a[5:2:-1])
+#
+print(sum(a))
+#sum
+print(8 in a)
+# to check if 8 is in this list
+print(4 not in a)
+# to check if 4 is not in this list
