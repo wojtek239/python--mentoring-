5ff7553219fb6e7c007a03f05b09494f8b7bf900
wojtek239 <wojtekopelt1@wp.pl>
2023-05-23 16:13:14 +0200

    finished with exc 12 now

---
+++ python_basic/week_4/training4-6/exc_12.py
@@ -0,0 +1,8 @@
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i))
+
+for i in range(ord('Z'), ord('A') - 1, -1):
+    print(chr(i))
+
+# chr = konwersja miedzy kodami ASCII a znakami
+# ord = zwraca liczbę reprezentującą kod Unicode danego znaku.
