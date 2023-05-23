8be098b1d246ba31550a175286082d4875335188
wojtek239 <wojtekopelt1@wp.pl>
2023-05-23 12:56:24 +0200

    finished with exc 10 now

---
+++ python_basic/week_4/training4-6/exc_10.py
@@ -0,0 +1,23 @@
+import random
+opening_val = int(input("please enter opening value: "))
+closing_val = int(input("please enter closing value: "))
+
+guessed_number = random.randint(opening_val, closing_val)
+score = closing_val - opening_val
+guess = False
+while not guess:
+    users_guess = int(input("please take your guess: "))
+    if users_guess == guessed_number:
+        guess = True
+        print(f'bravo !', users_guess, "is a good guess\nYou have", score, "points")
+    elif users_guess < guessed_number:
+        score -= 1
+        print("your guess is too low, please try again: ")
+
+    elif users_guess > guessed_number:
+        score -= 1
+        print("your guess is too high, please try again: ")
+
+    if score <= 0:
+        print("lol u lost")
+        break
