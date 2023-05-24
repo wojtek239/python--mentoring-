80eafcb65fb0fe95f0f0a3c97b6076c069ccbfbd
wojtek239 <wojtekopelt1@wp.pl>
2023-05-24 15:56:17 +0200

    exc 5 for now

---
+++ python_basic/week_4/training8/exc_5.py
@@ -0,0 +1,7 @@
+orders = {"client_1335": {"dish_name": "chicken_soup", "rating": 5, "bill": 20.0}, "client_222": {"dessert_name":
+ "vanilla_icecream", "bill": 5.0}}
+print("client_1335: ")
+print(orders["client_1335"])
+print("client_222: ")
+print(orders["client_222"])
+
