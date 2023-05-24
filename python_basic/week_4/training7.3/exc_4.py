4ca14bb2d48034bb9caf8d44ce3e53585ae7a3d2
wojtek239 <wojtekopelt1@wp.pl>
2023-05-24 09:43:57 +0200

    exc 4 for now

---
+++ python_basic/week_4/training7.3/exc_4.py
@@ -0,0 +1,6 @@
+users_friends = input("please enter names of your friends divided by space pls: ").split()
+
+for name in users_friends:
+    print(f"hi {name}, how are you doing? ")
+    print(f"haven't seen you in a long time {name} ")
+    print(f"it's good to see you {name} ")
