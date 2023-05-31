9cda4caa73b2b29ee3111cb7ad5d664152bd514d
wojtek239 <wojtekopelt1@wp.pl>
2023-05-31 18:52:10 +0200

    exc with getting values done

---
+++ python_basic/week_4/training7.2(dictionary-extension)/exc_8(getting values).py
@@ -0,0 +1,4 @@
+dict = { "V":"S001", "VI": "S002", "VII": "S001", "VIII": "S005", "IX":"S005", "X":"S009", "XI":"S007" }
+unique_values = set(dict.values())
+for value in unique_values:
+    print(value)
