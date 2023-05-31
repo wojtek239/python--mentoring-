03cf672ed53aa1c4609025a22f0ce3a0867744f3
wojtek239 <wojtekopelt1@wp.pl>
2023-05-31 17:20:12 +0200

    feat/exc with text and keys done

---
+++ python_basic/week_4/training7.2(dictionary-extension)/exc_3(text with keys).py
@@ -0,0 +1,19 @@
+
+text = """
+Once upon a midnight dreary, while I pondered, weak and weary,
+Over many a quaint and curious volume of forgotten lore,
+While I nodded, nearly napping, suddenly there came a tapping,
+As of someone gently rapping, rapping at my chamber door.
+This visitor, I muttered, tapping at my chamber door -
+Only this, and nothing more.
+"""
+word_count = {}
+words = text.split()
+
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+print(word_count)
