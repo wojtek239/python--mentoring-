26d9227f3edd5c4a82ab6036d647445a5751315d
wojtek239 <wojtekopelt1@wp.pl>
2023-06-06 13:33:23 +0200

    feat: classes constructors

---
+++ python_basic/5_Classes/Classes_theory/Various_classes_constructors.py
@@ -0,0 +1,15 @@
+class Vechicle:
+    def __init__(self, wheels_number_, colour_):
+        self.wheels_number = wheels_number_
+        self.colour = colour_
+
+class Furniture:
+    def __init__(self, size_, price_ ):
+        self.size = size_
+        self.price = price_
+
+class House:
+    def __init__(self, localization_, price_, area_):
+        self.localization = localization_
+        self.price = price_
+        self.area = area_
