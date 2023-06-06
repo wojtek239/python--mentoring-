fe04284a28bbc460ef71194cc2d630e388894d27
wojtek239 <wojtekopelt1@wp.pl>
2023-06-06 15:23:25 +0200

    feat: different methods

---
+++ python_basic/5_Classes/Classes_theory/Methods.py
@@ -0,0 +1,10 @@
+class Vechicle:
+    def __init__(self, wheels_number_, colour_):
+        self.wheels_number = wheels_number_
+        self.colour = colour_
+
+    def no_arguments_method(self):
+        pass
+
+    def one_argument_method(self, argument):
+        pass
