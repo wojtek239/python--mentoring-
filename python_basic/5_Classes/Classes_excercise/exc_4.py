ce856099baf0cb968c9a916ca1bdb2d8496948a3
wojtek239 <wojtekopelt1@wp.pl>
2023-06-13 14:50:16 +0200

    feat: banc account classes

---
+++ python_basic/5_Classes/Classes_excercise/exc_4.py
@@ -0,0 +1,41 @@
+class BankAccount:
+    def __init__(self, account_nr_: int, owner_name_: str, balance_: float):
+        self.account_nr = account_nr_
+        self.owner_name = owner_name_
+        self.balance = balance_
+
+    def deposit(self, amount_: float):
+        amount_ = float(input("please enter amount: "))
+        provision = amount_ // 100 * 2
+        self.balance += amount_ - provision
+        print(f'after payment, which is: {amount_}, provision will be: {provision}')
+
+    def withdrawal(self, amount_: float):
+        if amount_ <= self.balance:
+            self.balance -= amount_
+            print("withdrawal done.")
+        else:
+            print("na bro. u broke")
+
+    def change_ownership(self, new_owner_: str):
+        self.new_owner = new_owner_
+
+    def display(self):
+
+        print(f'information about account: ')
+        print(f'account number is: {self.account_nr}')
+        print(f'owner name is: {self.owner_name}')
+        print(f'balance is: {self.balance}')
+
+
+account1 = BankAccount(1029103, "ja", 9999999)
+account1.display()
+
+account1.deposit(1)
+account1.display()
+
+account1.withdrawal(999999999999999999999999999)
+account1.display()
+
+account1.change_ownership("obiwan kenobi")
+account1.display()
