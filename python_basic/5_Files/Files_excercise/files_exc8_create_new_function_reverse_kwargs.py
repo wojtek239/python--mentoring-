b430c4111cfadb918928e623943831591320e368
wojtek239 <wojtekopelt1@wp.pl>
2023-06-14 15:41:37 +0200

    feat: data.json excercise

--- python_basic/5_Files/Files_excercise/files_exc8_create_new_function_reverse_kwargs.py
+++ python_basic/5_Files/Files_excercise/files_exc8_create_new_function_reverse_kwargs.py
@@ -1,27 +1,13 @@
 import json
 
+with open('data.json') as file:
+    data = json.load(file)
 
-def reverse_dict(**kwargs: dict) -> dict[str, str]:
-    new_dict = {}
-
-    for k, v in kwargs.items():
-        new_dict[v] = k
-
-    return new_dict
-
-
-def main():
-    songs = {
-        'The Sensual World': 'Kate Bush',
-        'Shaday': 'Ofra Haza',
-        'Achtung Baby': 'U2',
-        'Aion': 'Dead Can Dance',
-        'Invisible Touch': 'Genesis'
-    }
-    data = reverse_dict(**songs)
-    with open("output.json", "w") as f:
-        json.dump(data, f)
+for package in data['packages']:
+    dn = package['DN']
+    description = package['Description']
+    speed = package['Speed']
+    mtu = package['MTU']
 
 
-if __name__ == "__main__":
-    main()
+print(f'{dn}, {description}, {speed}, {mtu}')
