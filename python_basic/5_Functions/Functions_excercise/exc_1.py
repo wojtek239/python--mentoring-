d1b3369a9a2b1e6b543bb30dbcd7c2d294bc35a5
wojtek239 <wojtekopelt1@wp.pl>
2023-06-14 17:37:05 +0200

    feat:exc 1 biggest index done correctly

--- python_basic/5_Functions/Functions_excercise/exc_1.py
+++ python_basic/5_Functions/Functions_excercise/exc_1.py
@@ -1,18 +1,11 @@
 
 nums = [4, 6, 8, 24, 12, 2]
 
+def find_max_index(nums):
+    max_value = max(nums)
+    for i, num in enumerate(nums):
+        if num == max_value:
+            return i
+index = find_max_index(nums)
+print(f'biggest index is: {index}')
 
-def find_max_index(nums_: int):
-    max_value = float('-inf')
-    max_index = None
-
-    for index, num in enumerate(nums):
-        if num > max_value:
-            max_value = num
-            max_index = index
-
-    return max_index
-
-
-max_index = find_max_index(nums)
-print(max_index)
