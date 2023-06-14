5e076e303914cdb243bdee39197056a92f254ff6
wojtek239 <wojtekopelt1@wp.pl>
2023-06-14 14:17:27 +0200

    feat: started exc 8 with case class

---
+++ python_basic/5_Classes/Classes_excercise/exc_8.py
@@ -0,0 +1,11 @@
+first_case = {
+    ‘name’: ‘first_case’,
+    ‘created_task’: ‘2021-10-26T19:48:12+00:00’,
+    ‘end_task’: None
+}
+
+second_case = {
+    ‘name’: ‘second_case’,
+    ‘created_task’: ‘2021-09-26T19:48:12+00:00’,
+    ‘end_task’: ‘2021-10-26T19:48:12+00:00’
+}
