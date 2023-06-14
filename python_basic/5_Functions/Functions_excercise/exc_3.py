fcf7299a8094cd072842b2b325326898b388e296
wojtek239 <wojtekopelt1@wp.pl>
2023-06-14 18:21:01 +0200

    feat: fizzbuzz function for numbers

---
+++ python_basic/5_Functions/Functions_excercise/exc_3.py
@@ -0,0 +1,13 @@
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+
+
+input_number = int(input("pls enter number:"))
+print(fizz_buzz(input_number))
