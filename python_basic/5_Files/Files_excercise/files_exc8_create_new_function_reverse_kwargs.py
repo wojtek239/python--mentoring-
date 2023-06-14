da3b172e97cffb9f61f83ae05f92343e1886ec47
wojtek239 <wojtekopelt1@wp.pl>
2023-06-14 17:05:19 +0200

    feat: exc 8 done, reversing kwargs

--- python_basic/5_Files/Files_excercise/files_exc8_create_new_function_reverse_kwargs.py
+++ python_basic/5_Files/Files_excercise/files_exc8_create_new_function_reverse_kwargs.py
@@ -1,13 +1,13 @@
 import json
 
-with open('data.json') as file:
-    data = json.load(file)
-
-for package in data['packages']:
-    dn = package['DN']
-    description = package['Description']
-    speed = package['Speed']
-    mtu = package['MTU']
+dictionary = {}
 
 
-print(f'{dn}, {description}, {speed}, {mtu}')
+def reverse_kwargs(**kwargs):
+    reversed_dict = {value: key for key, value in kwargs.items()}
+
+    with open('output.json', 'w') as file:
+        json.dump(reversed_dict, file)
+
+
+reverse_kwargs(klucz1='val1', klucz2='val2')
