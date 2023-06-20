a19b83344d8f5a68afca8aa63a082c37686a0672
wojtek239 <wojtekopelt1@wp.pl>
2023-06-20 11:11:04 +0200

    bugfix: correction with return placement

--- python_basic/5_Functions/Functions_excercise/exc_4.py
+++ python_basic/5_Functions/Functions_excercise/exc_4.py
@@ -4,12 +4,12 @@ def multiply_numbers(*args: int) -> int:
     multiply every number given
     """
     if len(args) == 0:
-        return None
+        return 0
 
     result = 1
     for num in args:
         result *= num
-        return result
+    return result
 
 
 user_input = input("please enter numbers: ")
