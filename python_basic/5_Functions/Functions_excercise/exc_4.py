8eec6caedae401df23fa8dc243ec08da80a6dc4d
wojtek239 <wojtekopelt1@wp.pl>
2023-06-20 09:16:58 +0200

    feat: excercise with multiplying numbers done

--- python_basic/5_Functions/Functions_excercise/exc_4.py
+++ python_basic/5_Functions/Functions_excercise/exc_4.py
@@ -1,3 +1,18 @@
-def elements_product(*args):
+
+def multiply_numbers(*args: int) -> int:
+    """
+    multiply every number given
+    """
     if len(args) == 0:
-        return 0
+        return None
+
+    result = 1
+    for num in args:
+        result *= num
+        return result
+
+
+user_input = input("please enter numbers: ")
+numbers = [int(num) for num in user_input.split()]
+
+print(multiply_numbers(*numbers))
