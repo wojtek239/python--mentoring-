977a95e0d207bbd6db0fe84d2993235d12eafb48
wojtek239 <wojtekopelt1@wp.pl>
2023-07-05 13:32:37 +0200

    feat/regex/exc5

---
+++ python_basic/7_regex/regex_exc/exc_5.py
@@ -0,0 +1,15 @@
+import re
+
+
+def find_string(string: str) -> list:
+    """
+    finds a string with max 6 letters and without a/A
+    """
+    str_set = r"\b(?=\w{6}\b)(?![Aa])\w+"
+    result = re.findall(str_set, string)
+    return result
+
+
+text = "unique dejifse eda addsfe njieadniadnaeijn 3443oij 4ke3"
+result = find_string(text)
+print(result)
