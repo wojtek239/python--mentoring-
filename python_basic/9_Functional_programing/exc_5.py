5258c483f2641e14f01119fbbffb0ddd2a740bea
wojtek239 <wojtekopelt1@wp.pl>
2023-07-19 17:13:21 +0200

    feat/functional/exc5 done

---
+++ python_basic/9_Functional_programing/exc_5.py
@@ -0,0 +1,11 @@
+from functools import reduce
+
+nums1 = [1, 2, 3]
+nums2 = [4, 5, 6]
+nums3 = [7, 8, 9]
+
+add =  lambda x, y: x + y
+
+result = reduce(add, nums1 + nums2 + nums3)
+
+print(f'Sum of these three lists is: ', result)
