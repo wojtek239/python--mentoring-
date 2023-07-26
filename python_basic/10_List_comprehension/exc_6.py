bae314edbf5b5da4085197f387abb55e2444d96e
wojtek239 <wojtekopelt1@wp.pl>
2023-07-27 00:15:32 +0200

    feat/ comprehension/ exc 5 done

---
+++ python_basic/10_List_comprehension/exc_6.py
@@ -0,0 +1,14 @@
+text = "The quick brown fox jumps over the lazy dog is an English-language pangram—a " \
+       "sentence that contains all of the letters of the English alphabet"
+
+# interpunkcja pierw ?
+import string
+translator = str.maketrans('', '', string.punctuation)
+text = text.translate(translator)
+
+words = text.split()
+
+
+length_of_words = [len(word) for word in words if word.lower() != "the"]
+
+print(length_of_words)
